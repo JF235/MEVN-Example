62c7dd89c3e9645a6766b436e7065933cc3288ae
João <jfcm.pipe@gmail.com>
2023-11-03 23:00:02 -0300

    Sistema login/cadastro, unificacao do DB - Melhorando a página de cadastro - Unificando DB funcionarios + logins - Incluir um funcionario = criar um login com cargo user - Agora um usuario pode ser cadastrado de maneira apropriada

--- AppTeste/routes/login.js
+++ AppTeste/routes/login.js
@@ -3,6 +3,20 @@ var router = express.Router();
 
 var loginsDB = require('../models/logins');
 
+function checkAuth(req, res) {
+  var cookies = req.cookies;
+  if (!cookies || !cookies.userAuth)
+    return 'unauthorized';
+  
+  var cauth = cookies.userAuth;
+  var content = JSON.parse(cauth);
+
+  if (content.key == 'secret' && content.cargo != undefined)
+    return content.cargo
+  else
+    return 'unauthorized';
+}
+
 function sendLoginFile(req, res) {  // GET
   var path = 'login.html';
   res.header('Cache-Control', 'no-cache');
@@ -25,7 +39,7 @@ async function realizarLogin(req, res) {
   }
 
   if (data.senha === req.body.senha) {
-    var content = { "cpf": req.body.cpf, "key": "secret" };
+    var content = { "key": "secret", "cargo": data.cargo };
     res.cookie("userAuth", JSON.stringify(content), { "maxAge": 3600000 * 24 * 5 });
     res.status(200).send("Sucesso");
   } else {
@@ -33,8 +47,18 @@ async function realizarLogin(req, res) {
   }
 }
 
+function logoutCurrentUser(req, res) {
+  if (checkAuth(req, res) != 'unauthorized') {
+    res.clearCookie('userAuth');	 // remove cookie no cliente
+    res.status(200).send('Sucesso');
+  } else {
+    res.status(401).send('Unauthorized');
+  }
+}
+
 router.route('/')
   .get(sendLoginFile)
   .post(realizarLogin)
+  .delete(logoutCurrentUser)
 
 module.exports = router;
