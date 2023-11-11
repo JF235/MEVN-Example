e694d83313d615fbe76c54695134e8698a03f35a
Nicolas Pereira da Silva <pereiranjobs@gmail.com>
2023-11-11 14:06:29 -0300

    mudando o menu

--- AppTeste/public/vue/header.js
+++ AppTeste/public/vue/header.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 async function goToLogin() {
   console.log("Redirecting...")
 
@@ -30,4 +31,41 @@ export default header =  {
     doGoToHome: goToHome,
     doGoToLogin: goToLogin, 
   },
+=======
+async function goToLogin() {
+  console.log("Redirecting...")
+
+  window.open("/login", "_self");
+}
+
+async function goToHome() {
+  console.log("Redirecting...")
+
+  window.open("/", "_self");
+}
+
+var header;
+export default header =  {
+  template: `
+  <div id="headercomponent">
+  <div id="ConteudoMenu">
+  <h1>
+  <a href="#" @click="doGoToHome">NOME DA EMPRESA</a>
+  </h1>
+  
+  <img src="images/transport-enterprise-leasing-companyupdate-1653680284062.png" alt="Caminhoes">
+  </div>
+  
+  <div class="menu">
+      <a class="link" @click="doGoToLogin">Login</a>
+  </div>
+
+  <br>
+  </div>
+  `,
+  methods: {
+    doGoToHome: goToHome,
+    doGoToLogin: goToLogin, 
+  },
+>>>>>>> Stashed changes
 };
