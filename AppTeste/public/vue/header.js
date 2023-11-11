bd573759f9d2fbb90ef9972acae47afd79fb5e5c
Nicolas Pereira da Silva <pereiranjobs@gmail.com>
2023-11-11 14:14:23 -0300

    aceitando incoming commits

--- AppTeste/public/vue/header.js
+++ AppTeste/public/vue/header.js
@@ -1,37 +1,3 @@
-<<<<<<< Updated upstream
-async function goToLogin() {
-  console.log("Redirecting...")
-
-  window.open("/login", "_self");
-}
-
-async function goToHome() {
-  console.log("Redirecting...")
-
-  window.open("/", "_self");
-}
-
-var header;
-export default header =  {
-  template: `
-  <div id="headercomponent">
-  <h1>
-  <a href="#" @click="doGoToHome">NOME DA EMPRESA</a>
-  </h1>
-  
-
-  <div class="menu">
-      <a class="link" @click="doGoToLogin">Login</a>
-  </div>
-
-  <br>
-  </div>
-  `,
-  methods: {
-    doGoToHome: goToHome,
-    doGoToLogin: goToLogin, 
-  },
-=======
 async function goToLogin() {
   console.log("Redirecting...")
 
@@ -67,5 +33,4 @@ export default header =  {
     doGoToHome: goToHome,
     doGoToLogin: goToLogin, 
   },
->>>>>>> Stashed changes
 };
