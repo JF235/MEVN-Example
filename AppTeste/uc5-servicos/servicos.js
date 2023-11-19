d82e903af5143961c176df2e4c33428524768178
João <jfcm.pipe@gmail.com>
2023-11-19 16:52:17 -0300

    Consulta e Escolha de Viagem

--- AppTeste/uc5-servicos/servicos.js
+++ AppTeste/uc5-servicos/servicos.js
@@ -1,6 +1,9 @@
 var express = require('express');
 var router = express.Router();
 
+const { checkAuth } = require('../ucx-auxiliar/checkAuth');
+
+var viagensDB = require('../models/viagens');
 
 function sendSolEntregaFile(req, res) {  // GET
     res.header('Cache-Control', 'no-cache');
@@ -16,6 +19,25 @@ function sendCompraPassagFile(req, res) {  // GET
     res.sendFile(path, { "root": "./" });
 }
 
+async function buscarViagem(req, res) {
+    if (checkAuth(req, res) != 'user') {
+        res.status(401).json({ 'resultado': 'Unauthorized' });
+        return;
+    }
+
+    res.header('Cache-Control', 'no-cache');
+
+    var response = {};
+
+    try {
+        var data = await viagensDB.find({"chegada": req.body.chegada, "partida": req.body.partida});
+        response = { "viagens": data };
+    } catch (err) {
+        response = { "resultado": "falha de acesso ao BD" };
+    }
+    res.json(response);
+}
+
 function sendEscolhaViagemFile(req, res) {  // GET
     res.header('Cache-Control', 'no-cache');
 
@@ -24,7 +46,8 @@ function sendEscolhaViagemFile(req, res) {  // GET
 }
 
 router.route('/escolhaViagem')
-    .get(sendEscolhaViagemFile);
+    .get(sendEscolhaViagemFile)
+    .post(buscarViagem);
 
 router.route('/solicitacaoEntrega')
     .get(sendSolEntregaFile);
